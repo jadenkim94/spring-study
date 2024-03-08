51daa93da374bd6439bbb816e70eb2effa7c0758
jaden94 <jonghaha7@gmail.com>
2024-03-08 21:08:14 +0900

    Propagation.REQUIRES_NEW option example

--- src/main/java/me/jaden/springstudy/transaction/InnerService.java
+++ src/main/java/me/jaden/springstudy/transaction/InnerService.java
@@ -2,6 +2,7 @@ package me.jaden.springstudy.transaction;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
@@ -12,7 +13,7 @@ public class InnerService {
 
     private final MemberRepository memberRepository;
 
-    @Transactional
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void makeNameUpper(Long id) {
         Optional<Member> targetMember = memberRepository.findById(id);
         targetMember.ifPresent(member -> member.makeNameUpper());
